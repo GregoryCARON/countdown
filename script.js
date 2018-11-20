cb835e41c48bbb3433955601649e21cbc30f56db
GregoryCARON <gregory.caron1@gmail.com>
2018-11-20 15:54:04 +0100

    Initial commit

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-var heures = 0;
-var minutes = 0;
-var secondes = 0;
-var temps = 0;
-var tempsFinal = 0;
+let heures = 0, minutes = 0, secondes = 0;
+let temps = 0;
+let tempsFinal = 0;
+var timerBoot = 0;
+var h = 0, m = 0, s = 0;
+var MP3 = 'GVFouet.mp3';
 
-var timer;
+document.getElementById('userHeures').innerHTML = '0';
+document.getElementById('userMinutes').innerHTML = '0';
+document.getElementById('userSecondes').innerHTML = '0';
+
+let timer;
+
 /*
 function convertTime(temps) {
     temps = parseInt(temps);
@@ -21,34 +27,132 @@ function convertTime(temps) {
 }
 
 */
+function lecture(son, vol, loop) {
+    let snd = new Audio();
+    snd.src = son;
+    snd.loop = loop;
+    snd.volume = vol;
+    snd.play();
+}
 
-
-var countdown = function() {
-    tempsFinal = parseInt(document.getElementById('temps').innerHTML);
-    temps = parseInt(document.getElementById('temps').innerHTML);
+var countdown = function () {
+    temps = tempsFinal;
+    console.log('temp : ' + temps + ', tempsFinal : ' + tempsFinal);
     heures = Math.floor(temps / 3600);
     temps = temps - (3600 * heures);
+    console.log('temps : ' + temps + ', heure : ' + heures);
     minutes = Math.floor(temps / 60);
     temps = temps - (60 * minutes);
     secondes = temps;
+    console.log('temps : ' + temps + ', minutes : ' + minutes + ', secondes : ' + secondes);
 
-    secondes--;
     tempsFinal--;
-    document.getElementById('temps').innerHTML = tempsFinal;
+
+    if (heures < 0) {
+        heures = 0;
+    }
+    if (minutes < 0) {
+        minutes = 0;
+    }
+    if (secondes < 0) {
+        secondes = 0;
+    }
 
 
+    document.getElementById('userHeures').innerHTML = heures;
+    document.getElementById('userMinutes').innerHTML = minutes;
+    document.getElementById('userSecondes').innerHTML = secondes;
 
-
-
-
-    timer = setTimeout(countdown, 1000);
+    if (heures === 0 && minutes === 0 && secondes === 0) {
+        document.getElementById('Start').innerHTML = 'Start';
+        clearTimeout(timer);
+        timerBoot = 0;
+        lecture(MP3, 1, 0);
+        alert('BOUM !!!');
+    } else {
+        timer = setTimeout(countdown, 1000);
+        timerBoot = 1;
+    }
 };
 
-document.getElementById('Start').addEventListener('click', countdown);
+document.getElementById('Start').addEventListener('click', function () {
+    if (timerBoot === 1) {
+        document.getElementById('Start').innerHTML = 'Start';
+        clearTimeout(timer);
+        timerBoot = 0;
+    } else {
+        document.getElementById('Start').innerHTML = 'Stop';
+        //tempsFinal = (h * 3600) + (m * 60) + s;
+        console.log(document.getElementById('userSecondes').value);
+        countdown();
+    }
+    console.log(timer);
+});
 
-
-
-
-
-
-//convertTime(5000);
+document.getElementById('btnAddHeure').addEventListener('click', function () {
+    h = parseInt(document.getElementById('userHeures').innerHTML);
+    h = h + 1;
+    tempsFinal = (h * 3600) + (m * 60) + s;
+    document.getElementById('userHeures').innerHTML = h;
+});
+document.getElementById('btnRemHeure').addEventListener('click', function () {
+    h = parseInt(document.getElementById('userHeures').innerHTML);
+    h = h - 1;
+    if (h < 0) {
+        h = 0;
+    }
+    tempsFinal = (h * 3600) + (m * 60) + s;
+    document.getElementById('userHeures').innerHTML = h;
+});
+document.getElementById('btnAddMinute').addEventListener('click', function () {
+    m = parseInt(document.getElementById('userMinutes').innerHTML);
+    m = m + 1;
+    if (m === 60) {
+        m = 0;
+        h = h + 1;
+        tempsFinal = (h * 3600) + (m * 60) + s;
+        document.getElementById('userHeures').innerHTML = h;
+    }
+    tempsFinal = (h * 3600) + (m * 60) + s;
+    document.getElementById('userMinutes').innerHTML = m;
+});
+document.getElementById('btnRemMinute').addEventListener('click', function () {
+    m = parseInt(document.getElementById('userMinutes').innerHTML);
+    m = m - 1;
+    if (m === -1) {
+        m = 59;
+        if (h > 0) {
+            h = h - 1;
+            tempsFinal = (h * 3600) + (m * 60) + s;
+            document.getElementById('userHeures').innerHTML = h;
+        }
+    }
+    tempsFinal = (h * 3600) + (m * 60) + s;
+    document.getElementById('userMinutes').innerHTML = m;
+});
+document.getElementById('btnAddSeconde').addEventListener('click', function () {
+    s = parseInt(document.getElementById('userSecondes').innerHTML);
+    s = s + 1;
+    if (s === 60) {
+        s = 0;
+        m = m + 1;
+        tempsFinal = (h * 3600) + (m * 60) + s;
+        document.getElementById('userMinutes').innerHTML = m;
+    }
+    tempsFinal = (h * 3600) + (m * 60) + s;
+    document.getElementById('userSecondes').innerHTML = s;
+});
+document.getElementById('btnRemSeconde').addEventListener('click', function () {
+    s = parseInt(document.getElementById('userSecondes').innerHTML);
+    s = s - 1;
+    if (s === -1) {
+        s = 59;
+        if (m > 0) {
+            m = m - 1;
+            tempsFinal = (h * 3600) + (m * 60) + s;
+            document.getElementById('userMinutes').innerHTML = m;
+        }
+    }
+    tempsFinal = (h * 3600) + (m * 60) + s;
+    document.getElementById('userSecondes').innerHTML = s;
+});
